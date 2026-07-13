59a7743601d78789e9504fca40aa3a7d0f5bbdfe
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-07-13 13:52:37 -0300

    Cria array de produtos da loja virtual

---
+++ js/lista.produtos.js
@@ -0,0 +1,137 @@
+const produtos = [
+    {
+        id_produto: 1,
+        descricao_produto: "Ferramentas",
+        valor_unitario: 500.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/ferramentas.png",
+        caminho_secao: "#ferramentas",
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 2,
+        descricao_produto: "Torquímetro",
+        valor_unitario: 739.90,
+        unidade: "UN",
+        caminho_imagem: "imagens/torquimetro.png",
+        caminho_secao: "#ferramentas",
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 3,
+        descricao_produto: "Faróis",
+        valor_unitario: 349.99,
+        unidade: "UN",
+        caminho_imagem: "imagens/farois.png",
+        caminho_secao: "#iluminacao",
+        secao: "Iluminação"
+    },
+    {
+        id_produto: 4,
+        descricao_produto: "Bateria",
+        valor_unitario: 500.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/bateria.png",
+        caminho_secao: "#eletrica",
+        secao: "Elétrica"
+    },
+    {
+        id_produto: 5,
+        descricao_produto: "Jogo de Pneus",
+        valor_unitario: 1119.90,
+        unidade: "JOGO",
+        caminho_imagem: "imagens/pneus.png",
+        caminho_secao: "#pneus",
+        secao: "Pneus"
+    },
+    {
+        id_produto: 6,
+        descricao_produto: "Chave de Boca",
+        valor_unitario: 69.90,
+        unidade: "UN",
+        caminho_imagem: "imagens/chave_de_boca.png",
+        caminho_secao: "#ferramentas",
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 7,
+        descricao_produto: "Vetores",
+        valor_unitario: 329.49,
+        unidade: "UN",
+        caminho_imagem: "imagens/vetores.png",
+        caminho_secao: "#equipamentos",
+        secao: "Equipamentos"
+    },
+    {
+        id_produto: 8,
+        descricao_produto: "Multímetro",
+        valor_unitario: 679.79,
+        unidade: "UN",
+        caminho_imagem: "imagens/multimetro.png",
+        caminho_secao: "#eletrica",
+        secao: "Elétrica"
+    },
+    {
+        id_produto: 9,
+        descricao_produto: "Cavalete",
+        valor_unitario: 449.99,
+        unidade: "PAR",
+        caminho_imagem: "imagens/cavalete.png",
+        caminho_secao: "#equipamentos",
+        secao: "Equipamentos"
+    },
+    {
+        id_produto: 10,
+        descricao_produto: "Macaco Hidráulico",
+        valor_unitario: 1329.90,
+        unidade: "UN",
+        caminho_imagem: "imagens/macaco_hidraulico.png",
+        caminho_secao: "#equipamentos",
+        secao: "Equipamentos"
+    },
+    {
+        id_produto: 11,
+        descricao_produto: "Carrinho Esteira",
+        valor_unitario: 229.90,
+        unidade: "UN",
+        caminho_imagem: "imagens/carrinho_esteira.png",
+        caminho_secao: "#equipamentos",
+        secao: "Equipamentos"
+    },
+    {
+        id_produto: 12,
+        descricao_produto: "Carrinho para Ferramentas",
+        valor_unitario: 684.99,
+        unidade: "UN",
+        caminho_imagem: "imagens/carrinho_para_ferramentas.png",
+        caminho_secao: "#equipamentos",
+        secao: "Equipamentos"
+    },
+    {
+        id_produto: 13,
+        descricao_produto: "Motor de Partida",
+        valor_unitario: 1934.24,
+        unidade: "UN",
+        caminho_imagem: "imagens/motor_de_partida.png",
+        caminho_secao: "#motores",
+        secao: "Motores"
+    },
+    {
+        id_produto: 14,
+        descricao_produto: "Alternador Toyota Corolla 2019 em diante",
+        valor_unitario: 1050.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/alternador.png",
+        caminho_secao: "#eletrica",
+        secao: "Elétrica"
+    },
+    {
+        id_produto: 15,
+        descricao_produto: "Jogo de 3 Alicates Gedore",
+        valor_unitario: 124.90,
+        unidade: "JOGO",
+        caminho_imagem: "imagens/kit_3_alicates.png",
+        caminho_secao: "#ferramentas",
+        secao: "Ferramentas"
+    }
+];
